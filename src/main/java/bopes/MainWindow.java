cb554b454d7bf69e1638ebce856852ae5546460e
keithxun <keithonghx@gmail.com>
2024-09-12 16:37:42 +0800

    Remove unusued import and test workflow

--- src/main/java/bopes/MainWindow.java
+++ src/main/java/bopes/MainWindow.java
@@ -6,7 +6,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class MainWindow {
 
